2de5c703aa83caa1c2036dcc20b49d2157a9a678
Jasper Paul Krol <jasper.paul.krol@gmail.com>
2021-11-03 17:10:12 +0100

    first commit

---
+++ src/test/java/nl/library/librarbyExcercise/LibrarbyExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.library.librarbyExcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarbyExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
